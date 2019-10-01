3fed78fed87d0d3be80dd9531ea91921d2b1a2b9
Cristian Fonseca <cristian.lfs@gmail.com>
2019-09-30 22:05:54 -0500

    Updates

---
+++ precache-manifest.537482951b01956d32a2301e98b88998.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4fc90ff9bcf207788002b0b7c142320",
+    "url": "/alejandrofonseca/index.html"
+  },
+  {
+    "revision": "5726b6a485b24f0d02da",
+    "url": "/alejandrofonseca/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "5726b6a485b24f0d02da",
+    "url": "/alejandrofonseca/static/js/2.d8b7e5fd.chunk.js"
+  },
+  {
+    "revision": "ea995ca9b200be97e72c",
+    "url": "/alejandrofonseca/static/js/main.d3fb4e4b.chunk.js"
+  },
+  {
+    "revision": "b268f9209ebdb01f1e70",
+    "url": "/alejandrofonseca/static/js/runtime-main.d5138ed4.js"
+  }
+]);
